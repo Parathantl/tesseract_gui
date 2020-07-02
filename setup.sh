cdbe90e1a4adb7cdbdfe07d7183c912b70c7c948
parathantl <parathanlive123@gmail.com>
2020-07-02 09:25:36 +0530

    Addes Shell Script to run GUI in Linux distribution OS

---
+++ setup.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+sudo apt update
+
+#Python Pip3
+sudo apt install python3-pip
+
+# Ghost Script
+sudo apt install ghostscript
+
+# Install PyQT5 for GUI
+pip3 install PyQT5
+
+# Install Python PyPDF2 Module
+pip3 install PyPDF2
+
+# Install Tesseract
+sudo apt install tesseract-ocr
+
+# Install All Available Languages
+#sudo apt-get install tesseract-ocr-all
+
+# Install Tamil Language 
+sudo apt-get install tesseract-ocr-tam
